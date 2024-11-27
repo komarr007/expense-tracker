1403217f3f3a83248e43b62b4d744a5c5d066248
komarr007 <mariorangga000@gmail.com>
2024-11-27 20:37:08 +0700

    adjust bar chart

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -16,7 +16,6 @@ class _DashboardScreenState extends State<DashboardScreen> {
   double _monthlyTotalExpense = 0.0;
   Expense? _biggestExpense;
   Map<String, double> _categoryTotals = {};
-  List<FlSpot> _weeklyExpenseSpots = [];
 
   final currencyFormatter = NumberFormat.currency(locale: 'id_ID', symbol: 'Rp', decimalDigits: 0);
 
@@ -27,22 +26,18 @@ class _DashboardScreenState extends State<DashboardScreen> {
   }
 
   Future<void> _fetchExpenses() async {
-    // Fetch all expenses from the database
     List<Expense> expenses = await DBHelper().getExpenses();
-
     setState(() {
       _expenses = expenses;
       _calculateMonthlyTotalExpense();
       _calculateBiggestExpense();
       _calculateCategoryTotals();
-      _calculateWeeklyExpenses();
     });
   }
 
   void _calculateMonthlyTotalExpense() {
     DateTime now = DateTime.now();
     DateTime startOfMonth = DateTime(now.year, now.month, 1);
-    
     _monthlyTotalExpense = _expenses
         .where((expense) => expense.spend_date.isAfter(startOfMonth))
         .fold(0.0, (sum, expense) => sum + expense.amount);
@@ -65,22 +60,6 @@ class _DashboardScreenState extends State<DashboardScreen> {
     }
   }
 
-  void _calculateWeeklyExpenses() {
-    DateTime now = DateTime.now();
-    List<FlSpot> spots = [];
-    for (int i = 6; i >= 0; i--) {
-      DateTime day = now.subtract(Duration(days: i));
-      double dailyTotal = _expenses
-          .where((expense) =>
-              expense.spend_date.year == day.year &&
-              expense.spend_date.month == day.month &&
-              expense.spend_date.day == day.day)
-          .fold(0.0, (sum, expense) => sum + expense.amount);
-      spots.add(FlSpot(6 - i.toDouble(), dailyTotal));
-    }
-    _weeklyExpenseSpots = spots;
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -98,7 +77,7 @@ class _DashboardScreenState extends State<DashboardScreen> {
             SizedBox(height: 16),
             _buildCategoryDistributionChart(),
             SizedBox(height: 16),
-            _buildWeeklyExpenseTrendChart(),
+            _buildMonthlyExpenseBarChart(),
           ],
         ),
       ),
@@ -110,15 +89,15 @@ class _DashboardScreenState extends State<DashboardScreen> {
       scrollDirection: Axis.horizontal,
       child: Row(
         children: [
-          _buildSummaryCard('Total Expenses', currencyFormatter.format(_monthlyTotalExpense), const Color.fromARGB(255, 255, 255, 255)),
+          _buildSummaryCard('Total Expenses', currencyFormatter.format(_monthlyTotalExpense), Colors.white),
           _buildSummaryCard(
-            'Biggest Expense', 
-            _biggestExpense != null 
-              ? '${_biggestExpense!.name}: ${currencyFormatter.format(_biggestExpense!.amount)}' 
-              : 'No expenses',
-            const Color.fromARGB(255, 255, 255, 255),
+            'Biggest Expense',
+            _biggestExpense != null
+                ? '${_biggestExpense!.name}: ${currencyFormatter.format(_biggestExpense!.amount)}'
+                : 'No expenses',
+            Colors.white,
           ),
-          _buildSummaryCard('Number of Expenses', '${_expenses.length}', const Color.fromARGB(255, 255, 255, 255)),
+          _buildSummaryCard('Number of Expenses', '${_expenses.length}', Colors.white),
         ],
       ),
     );
@@ -144,39 +123,16 @@ class _DashboardScreenState extends State<DashboardScreen> {
   }
 
   Widget _buildCategoryDistributionChart() {
-    List<PieChartSectionData> sections = _categoryTotals.entries
-        .map((entry) {
-          double total = _categoryTotals.values.fold(0, (sum, value) => sum + value);
-          double percentage = (entry.value / total) * 100;
-          return PieChartSectionData(
-            value: entry.value,
-            title: '${percentage.toStringAsFixed(1)}%', // Only show the percentage on the chart
-            color: _getCategoryColor(entry.key),
-            radius: 50,
-            badgeWidget: GestureDetector(
-              onTap: () => _showCategoryInfo(entry.key, entry.value),
-              child: Container(
-                width: 24,
-                height: 24,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  shape: BoxShape.circle,
-                ),
-                child: Center(
-                  child: Text(
-                    '${percentage.toStringAsFixed(1)}%',
-                    style: TextStyle(
-                      fontSize: 10,
-                      color: Colors.black,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            badgePositionPercentageOffset: 0.98, // Position it near the edge
-          );
-        })
-        .toList();
+    List<PieChartSectionData> sections = _categoryTotals.entries.map((entry) {
+      double total = _categoryTotals.values.fold(0, (sum, value) => sum + value);
+      double percentage = (entry.value / total) * 100;
+      return PieChartSectionData(
+        value: entry.value,
+        title: '${percentage.toStringAsFixed(1)}%',
+        color: _getCategoryColor(entry.key),
+        radius: 50,
+      );
+    }).toList();
 
     return Container(
       height: 200,
@@ -196,96 +152,124 @@ class _DashboardScreenState extends State<DashboardScreen> {
     );
   }
 
-  void _showCategoryInfo(String category, double total) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return AlertDialog(
-          backgroundColor: Colors.blueGrey[900],
-          title: Text(
-            'Category Info',
-            style: TextStyle(color: Colors.white),
-          ),
-          content: Text(
-            '$category: ${currencyFormatter.format(total)}',
-            style: TextStyle(color: Colors.white70),
-          ),
-          actions: [
-            TextButton(
-              onPressed: () => Navigator.of(context).pop(),
-              child: Text(
-                'Close',
-                style: TextStyle(color: Colors.blue),
-              ),
-            ),
-          ],
-        );
-      },
-    );
-  }
-  
   Color _getCategoryColor(String category) {
     switch (category) {
       case 'jajan':
-        return const Color.fromARGB(255, 27, 85, 50);
-      case 'makan':
-        return const Color.fromARGB(255, 33, 29, 138);
-      case 'others':
-        return const Color.fromARGB(255, 114, 60, 27);
-      case 'investment':
-        return const Color.fromARGB(255, 29, 65, 67);
-      case 'health':
-        return const Color.fromARGB(255, 28, 147, 90);
-      case 'tarik tunai':
-        return const Color.fromARGB(255, 64, 62, 61);
-      case 'saving':
-        return const Color.fromARGB(255, 126, 126, 125);
-      case 'mandatory share income':
-        return const Color.fromARGB(255, 74, 1, 84);
-      default:
         return Colors.green;
+      case 'makan':
+        return Colors.blue;
+      case 'others':
+        return Colors.orange;
+      default:
+        return Colors.grey;
     }
   }
 
-  Widget _buildWeeklyExpenseTrendChart() {
+  Widget _buildMonthlyExpenseBarChart() {
+    Map<String, double> monthlyExpenses = {};
+    for (var expense in _expenses) {
+      String month = DateFormat('MMM yyyy').format(expense.spend_date);
+      monthlyExpenses.update(
+        month,
+        (value) => value + expense.amount,
+        ifAbsent: () => expense.amount,
+      );
+    }
+
+    List<BarChartGroupData> barGroups = monthlyExpenses.entries.map((entry) {
+      int index = monthlyExpenses.keys.toList().indexOf(entry.key);
+      return BarChartGroupData(
+        x: index,
+        barsSpace: 50,
+        barRods: [
+          BarChartRodData(
+            toY: entry.value,
+            gradient: LinearGradient(
+              colors: [Colors.greenAccent, Colors.green],
+              begin: Alignment.bottomCenter,
+              end: Alignment.topCenter,
+            ),
+            width: 30,
+            borderRadius: BorderRadius.circular(6),
+          ),
+        ],
+      );
+    }).toList();
+
     return Container(
-      height: 200,
+      height: 250,
       padding: const EdgeInsets.all(16.0),
       decoration: BoxDecoration(
         color: Colors.blueGrey[800],
         borderRadius: BorderRadius.circular(8.0),
       ),
-      child: LineChart(
-        LineChartData(
-          titlesData: FlTitlesData(
-            leftTitles: AxisTitles(
-              sideTitles: SideTitles(showTitles: true, reservedSize: 30),
-            ),
-            bottomTitles: AxisTitles(
-              sideTitles: SideTitles(
-                showTitles: true,
-                getTitlesWidget: (value, meta) {
-                  final day = DateTime.now().subtract(Duration(days: 6 - value.toInt()));
-                  return Text('${day.day}/${day.month}');
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Container(
+          width: barGroups.length * 70.0,
+          child: BarChart(
+            BarChartData(
+              barGroups: barGroups,
+              maxY: monthlyExpenses.values.isNotEmpty
+                  ? (monthlyExpenses.values.reduce((a, b) => a > b ? a : b) * 1.2)
+                  : 0.0,
+              titlesData: FlTitlesData(
+                leftTitles: AxisTitles(
+                  sideTitles: SideTitles(
+                    showTitles: false,
+                    reservedSize: 50,
+                    getTitlesWidget: (value, meta) {
+                      return Text(
+                        currencyFormatter.format(value),
+                        style: TextStyle(color: Colors.white70, fontSize: 10),
+                      );
+                    },
+                  ),
+                ),
+                bottomTitles: AxisTitles(
+                  sideTitles: SideTitles(
+                    // reservedSize: 22,
+                    showTitles: true,
+                    // interval: 5,
+                    getTitlesWidget: (value, meta) {
+                      int index = value.toInt();
+                      if (index < 0 || index >= monthlyExpenses.keys.length) {
+                        return Container(); // Avoid out-of-bounds errors
+                      }
+                      return Padding(
+                        padding: const EdgeInsets.only(top: 8.0),
+                        child: Text(
+                          monthlyExpenses.keys.elementAt(index),
+                          style: TextStyle(color: Colors.white70, fontSize: 10),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
+              gridData: FlGridData(
+                show: true,
+                drawHorizontalLine: true,
+                getDrawingHorizontalLine: (value) {
+                  return FlLine(color: Colors.grey, strokeWidth: 0.5);
                 },
-                interval: 1,
+              ),
+              borderData: FlBorderData(show: false),
+              barTouchData: BarTouchData(
+                enabled: true,
+                touchTooltipData: BarTouchTooltipData(
+                  getTooltipItem: (group, groupIndex, rod, rodIndex) {
+                    return BarTooltipItem(
+                      '${monthlyExpenses.keys.elementAt(group.x)}\n${currencyFormatter.format(rod.toY)}',
+                      TextStyle(color: const Color.fromARGB(255, 255, 255, 255), fontWeight: FontWeight.bold),
+                    );
+                  },
+                ),
               ),
             ),
           ),
-          gridData: FlGridData(show: true),
-          borderData: FlBorderData(show: true),
-          minY: 0,
-          lineBarsData: [
-            LineChartBarData(
-              spots: _weeklyExpenseSpots,
-              isCurved: true,
-              color: Colors.green,
-              barWidth: 4,
-              dotData: FlDotData(show: true),
-            ),
-          ],
         ),
-      ),
+      )
     );
   }
 }
